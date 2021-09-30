fc17b14cf45a1eaab8ee2d1e3304814aebf6c0c4
tjm8975 <tjm8975@g.rit.edu>
2021-09-30 15:46:43 -0400

    Added tests for is_book_available

--- tests/test_ext_api_interface.py
+++ tests/test_ext_api_interface.py
@@ -15,13 +15,25 @@ class TestExtApiInterface(unittest.TestCase):
 
     def test_make_request_True(self):
         attr = {'json.return_value': dict()}
-        requests.get = Mock(return_value = Mock(status_code = 200, **attr))
+        requests.get = Mock(return_value=Mock(status_code=200, **attr))
         self.assertEqual(self.api.make_request(""), dict())
 
     def test_make_request_False(self):
         attr = {'json.return_value': dict()}
-        requests.get = Mock(return_value = Mock(status_code = 100, **attr))
+        requests.get = Mock(return_value=Mock(status_code=100, **attr))
         self.assertEqual(self.api.make_request(""), None)
+
+    def test_is_book_available_True(self):
+        self.api.make_request = Mock(return_value=self.json_data)
+        self.assertTrue(self.api.is_book_available(self.book))
+
+    def test_is_book_available_False(self):
+        self.api.make_request = Mock(return_value=dict())
+        self.assertFalse(self.api.is_book_available(self.book))
+
+    def test_is_book_available_None(self):
+        self.api.make_request = Mock(return_value=None)
+        self.assertFalse(self.api.is_book_available(self.book))
 
     def test_get_ebooks(self):
         self.api.make_request = Mock(return_value=self.json_data)
