730a2a56910bf930dcc582198443cfbf63a077fe
Nolan Porter <nrp4137@rit.edu>
2021-10-03 15:53:50 -0400

    Need help

--- tests/test_library.py
+++ tests/test_library.py
@@ -9,9 +9,12 @@ class TestLibrary(unittest.TestCase):
         library.Library_DB = Mock()
         library.Books_API  = Mock()
         self.lib = library.Library()
+        self.author = "Rick Riordan"
         # self.books_data = [{'title': 'Learning Python', 'ebook_count': 3}, {'title': 'Learning Python (Learning)', 'ebook_count': 1}, {'title': 'Learning Python', 'ebook_count': 1}, {'title': 'Learn to Program Using Python', 'ebook_count': 1}, {'title': 'Aprendendo Python', 'ebook_count': 1}, {'title': 'Python Basics', 'ebook_count': 1}]
         with open('tests_data/ebooks.txt', 'r') as f:
             self.books_data = json.loads(f.read())
+        with open('tests_data/author_data.txt', 'r') as f:
+            self.author_data = json.loads(f.read())
 
 
 
@@ -22,3 +25,16 @@ class TestLibrary(unittest.TestCase):
     def test_get_ebooks_count(self):
         self.lib.api.get_ebooks = Mock(return_value=self.books_data)
         self.assertEqual(self.lib.get_ebooks_count("learning python"), 8)
+
+
+    def test_is_book_by_author(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.author_data)
+        self.assertEqual(self.lib.is_book_by_author( ['rick riordan'], ["percy jackson 2"]), False)
+
+
+    '''
+    def test_get_langauges_for_book(self):
+        self.lib.api.get_book_info = Mock(return_value=self.json_data)
+        book = self.lib.api.get_book_info('learning python')
+        self.assertEqual(self.lib.
+    '''
